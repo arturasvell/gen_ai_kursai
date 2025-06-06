44932dc6311d03b789163631958d8ebc5b91953f
arturasvell <arturasvell@gmail.com>
2025-06-06 17:54:16 +0300

    enable more questioning

--- day8_apartment_bot/chatbot_06_06.py
+++ day8_apartment_bot/chatbot_06_06.py
@@ -8,17 +8,18 @@ MODEL_NAME = "gemini-2.0-flash"
 API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/{MODEL_NAME}:generateContent?key={API_KEY}"
 SYSTEM_PROMPT = "You are an expert on improving apartments. If the user asks anything outside that domain, answer: \"Sorry, I don't have knowledge on that.\" Wait for a new question."
 ON_TOPIC_KEYWORDS = ["apartment", "flat", "room", "renovation", "furniture", "storage", "decor"]
+FINISH_KEYWORDS = ["stop", "exit"]
 
 def is_question_on_topic(question: str) -> bool:
     question_lower = question.lower()
     return any(keyword in question_lower for keyword in ON_TOPIC_KEYWORDS)
 
-def ask_apartment_bot(question: str) -> None:
-    if not is_question_on_topic(question):
-        print("Your question is off-topic. Responding without API call.")
-        print("\nGEMINI: Sorry, I don't have knowledge on that.")
-        return
-    
+def is_exit_requested(question: str)->bool:
+    question_lower = question.lower()
+    return any(keyword in question_lower for keyword in FINISH_KEYWORDS)   
+
+
+def send_question_to_gemini(question: str):
     print("Sending API call to Google Gemini...")
     headers = {
         "Content-Type": "application/json",
@@ -53,8 +54,23 @@ def ask_apartment_bot(question: str) -> None:
     except RequestException as e:
         print(f"An error occurred with the API call: {e}")
         if e.response:
-            print(f"Error message: {e.response.text}")
+            print(f"Error message: {e.response.text}")  
+
+def ask_apartment_bot(question: str) -> None:
+    if not is_question_on_topic(question):
+        print("Your question is off-topic. Responding without API call.")
+        print("\nGEMINI: Sorry, I don't have knowledge on that.")
+        return
+    
+    send_question_to_gemini(question)
+    
 
 print("Hello. I am GEMINI. Please ask me questions related to apartments.")
-user_question = input("\nPlease enter your question: ")
-ask_apartment_bot(user_question)
+
+while True:
+    user_question = input("\nPlease enter your question: ")
+    if is_exit_requested(user_question):
+        print("Exit request received. Stopping the program.")
+        break
+    
+    ask_apartment_bot(user_question)
